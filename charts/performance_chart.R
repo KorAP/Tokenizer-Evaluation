36b5d508a08e983818da71b60ef3a2b3169e8685
Marc Kupietz <kupietz@ids-mannheim.de>
2022-03-17 21:50:22 +0100

    Add R scripts to plot performance charts
    Change-Id: I20d33a47722f515897c79e9e58c49f6391e48eac

---
+++ charts/performance_chart.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(idsThemeR) # install_git("https://korap.ids-mannheim.de/gerrit/IDS-Mannheim/idsThemeR")
+
+df <- read_tsv("performance.tsv")
+df %>% 
+  fill(Tool) %>%
+  mutate(order_by = pmax(.[[6]], .[[7]])) %>%
+  filter(Tool!="wc", !is.na(order_by)) %>%
+  pivot_longer(cols=c(7, 6)) %>%
+  mutate(name=str_replace_all(name, "Tokens/ms effi", "")) %>%
+  mutate(tool = paste0(Tool, if_else(is.na(Model), "", paste0(" (", Model, ")")))) %>%
+  { df2 <<- . } %>%
+  mutate(Tool= factor(tool) %>% fct_reorder(order_by)) %>%
+  ggplot(aes(x=Tool, y=value, fill=forcats::fct_rev(name))) + 
+  geom_col(position="dodge") + 
+  ylab("Tokens / ms") +
+  xlab(NULL) +
+  coord_flip() + 
+  theme_ids(style="light") +
+  theme(legend.position="bottom", legend.title = element_blank()) +
+  scale_fill_ids(palette = "d3")
+
+ggsave("tok_perf.png", width = 70 * .pt, height = 40 *.pt, units = "mm", dpi = 600)
